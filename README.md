f5bccad563beaab35b0e4739adbe37dd13ae11ba
Neil Patel <neil1023@yahoo.com>
2017-05-02 03:45:11 +0000

    Cleaned it up

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Analyzing climate change between the years 1950 and 2016 using the NASA Nex DCP-30 Dataset!
 
 This project looks at the major cities within the US and determines which cities revealed the greatest differences in temparature.
+
+To run this project, just call 'sh runAvgTempCities.sh'
--- runAvgTempCities.sh
+++ runAvgTempCities.sh
@@ -5,4 +5,6 @@ mkdir classes
 /usr/local/hadoop/bin/hadoop com.sun.tools.javac.Main -d classes AvgTempCities.java
 jar cf AvgTempCities.jar classes/*.class
 /usr/local/hadoop/bin/hadoop jar AvgTempCities.jar AvgTempCities /cities /avg_temp_cities/output_w_avgs
+hadoop fs -get /avg_temp_cities/output/part-r-00000
+mv part-r-00000 output_results
 python3 computeDeltas.py
